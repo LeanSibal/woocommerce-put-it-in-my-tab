283bf82a3e68eb771760c532a7d2adfc7d4aabd1
Lean Sibal <lsibal@apdgroup.com>
2018-09-15 21:42:10 +0800

    Initial plugin

---
+++ plugin.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Plugin Name: WooCommerce Put it on my tab
+ * Plugin URI: https://github.com/LeanSibal/woocommerce-put-it-in-my-tab
+ * Description: Allows Customers to accumulate orders and pay once a month.
+ * Author: Lean Sibal
+ * Author URI: https://github.com/LeanSibal/
+ * Version: 0.0.1
+ * Text Domain: woocommerce-put-it-on-my-tab
+ * Domain Path: /languages
+ */
